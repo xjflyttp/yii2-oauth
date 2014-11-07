e35087415d5502b26413c7661b138f1da70be4ed
lichunqiang <light-li@hotmail.com>
2014-11-07 12:10:11 +0800

    Update RenrenAuth.php

--- RenrenAuth.php
+++ RenrenAuth.php
@@ -4,6 +4,7 @@ use yii\authclient\OAuth2;
 /**
  * Renren OAuth
  * @author light <light-li@hotmail.com>
+ * @see http://wiki.dev.renren.com/wiki/Authentication
  */
 class RenrenAuth extends OAuth2 implements IAuth
 {
@@ -11,23 +12,40 @@ class RenrenAuth extends OAuth2 implements IAuth
      * @inheritdoc
      */
     public $authUrl = 'https://graph.renren.com/oauth/authorize';
+
     /**
      * @inheritdoc
      */
     public $tokenUrl = 'https://graph.renren.com/oauth/token';
+
     /**
      * @inheritdoc
      */
     public $apiBaseUrl = 'https://api.renren.com';
+
+    /**
+     * Try to use getUserAttributes to get simple user info
+     * @see http://wiki.dev.renren.com/wiki/Authentication
+     *
+     * @inheritdoc
+     */
+    protected function initUserAttributes()
+    {
+        return $this->getAccessToken()->getParams()['user'];
+    }
+
     /**
      * Get authed user info
      *
+     * @see http://wiki.dev.renren.com/wiki/V2/user/get
      * @return array
      */
     public function getUserInfo()
     {
-        return $this->getAccessToken()->getParams()['user'];
+        $user = $this->getUserAttributes();
+        return $this->api("v2/user/get", 'GET', ['userId' => $user['id']]);
     }
+
     /**
      * @inheritdoc
      */
